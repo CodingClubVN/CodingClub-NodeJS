0f3a96b31f5e75ac1949b539cc4ccb891bd815a8
Nguyễn Văn Lâm <ngocthau047@gmail.com>
2021-08-23 11:56:23 +0700

    code method delete api/friends/unfriend/:username_friend

--- routers/api/friends.js
+++ routers/api/friends.js
@@ -130,6 +130,25 @@ router.delete('/cancel/:username_friend',checkToken.checkToken ,async (req, res)
         res.status(400).json({message: err, success: false});
     }
 })
+router.delete('/unfriend/:username_friend',checkToken.checkToken,async (req, res) => {
+    try {
+        const token = req.headers['authorization'].split(' ')[1];
+        const user = jwt.verify(token, process.env.JWT_SECRET);
+        const friends = await Friends.findOne({id_user: user.id});
+        if (!friends) throw Error("Error!");
+        const user_friend = await Users.findOne({username: req.params.username_friend});
+        const array = friends.list_friends.filter(item => item !== user_friend._id.toString());
+        await Friends.updateOne(
+            {id_user: user.id},
+            {
+                $set: {list_friends: array}
+            }
+        )
+        res.status(200).json({message: "you unfriended" + user_friend.username,success: true});
+    }catch (err) {
+        res.status(400).json({message: err, success: false});
+    }
+})
 module.exports = router;
 
 
